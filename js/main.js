939d842fb4085df9412c167c501287bbc4a0625f
caasi Huang <caasi.igd@gmail.com>
2014-08-03 09:05:59 +0800

    add padding zeros

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 (function(){
-  var $wrap, config, update;
+  var $wrap, config, padding, update;
   $wrap = $('#wrap');
   config = {
     deadline: '2014-08-03T1600',
     flag: countdown.HOURS | countdown.MINUTES | countdown.SECONDS | countdown.MILLISECONDS
   };
+  padding = function(num, len){
+    var result, i$;
+    result = num + "";
+    for (i$ = result.length; i$ < len; ++i$) {
+      result = "0" + result;
+    }
+    return result;
+  };
   update = function(){
     var left;
     left = moment().countdown(config.deadline, config.flag);
-    $wrap.html(left.hours + ":" + left.minutes + ":" + left.seconds + "." + left.milliseconds);
+    $wrap.html(padding(left.hours, 2) + ":" + padding(left.minutes, 2) + ":" + padding(left.seconds, 2) + "." + padding(left.milliseconds, 3));
     requestAnimationFrame(update);
   };
   requestAnimationFrame(update);
--- src/ls/main.ls
+++ src/ls/main.ls
@@ -4,9 +4,15 @@ config =
   deadline: '2014-08-03T1600'
   flag: countdown.HOURS .|. countdown.MINUTES .|. countdown.SECONDS .|. countdown.MILLISECONDS
 
+padding = (num, len) ->
+  result = "#num"
+  for from result.length til len
+    result = "0#result"
+  result
+
 update = !->
   left = moment!countdown config.deadline, config.flag
-  $wrap.html "#{left.hours}:#{left.minutes}:#{left.seconds}.#{left.milliseconds}"
+  $wrap.html "#{padding left.hours, 2}:#{padding left.minutes, 2}:#{padding left.seconds, 2}.#{padding left.milliseconds, 3}"
   request-animation-frame update
 
 request-animation-frame update
