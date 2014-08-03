5abf83e053e9d47de5abf971c991b6da4ee0047f
caasi Huang <caasi.igd@gmail.com>
2014-08-03 08:44:40 +0800

    countdown timer for FGJ 2014

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 (function(){
-  console.log('foobar');
+  var $wrap, config, update;
+  $wrap = $('#wrap');
+  config = {
+    deadline: '2014-08-03T1600',
+    flag: countdown.HOURS | countdown.MINUTES | countdown.SECONDS | countdown.MILLISECONDS
+  };
+  update = function(){
+    var left;
+    left = moment().countdown(config.deadline, config.flag);
+    $wrap.html(left.hours + ":" + left.minutes + ":" + left.seconds + "." + left.milliseconds);
+    requestAnimationFrame(update);
+  };
+  requestAnimationFrame(update);
 }).call(this);
--- src/ls/main.ls
+++ src/ls/main.ls
@@ -1,1 +1,12 @@
-console.log \foobar
+$wrap = $ '#wrap'
+
+config =
+  deadline: '2014-08-03T1600'
+  flag: countdown.HOURS .|. countdown.MINUTES .|. countdown.SECONDS .|. countdown.MILLISECONDS
+
+update = !->
+  left = moment!countdown config.deadline, config.flag
+  $wrap.html "#{left.hours}:#{left.minutes}:#{left.seconds}.#{left.milliseconds}"
+  request-animation-frame update
+
+request-animation-frame update
