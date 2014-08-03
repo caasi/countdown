664c87bd5fce2bf54f459d4fb6aee2b949ee2f07
caasi Huang <caasi.igd@gmail.com>
2014-08-03 12:09:34 +0800

    add ending animation

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
 (function(){
-  var $time, config, padding, update;
+  var $time, config, padding, counter, zero, update;
   $time = $('#time');
   config = {
     deadline: '2014-08-03T1600',
-    flag: countdown.HOURS | countdown.MINUTES | countdown.SECONDS | countdown.MILLISECONDS
+    flag: countdown.HOURS | countdown.MINUTES | countdown.SECONDS | countdown.MILLISECONDS,
+    ending: '00:00:00.000'
   };
   padding = function(num, len){
     var result, i$;
+    len == null && (len = 2);
     result = num + "";
     for (i$ = result.length; i$ < len; ++i$) {
       result = "0" + result;
     }
     return result;
   };
+  counter = 0;
+  zero = function(){
+    $time.html(counter < 10
+      ? ''
+      : config.ending);
+    counter += 1;
+    counter %= 20;
+    requestAnimationFrame(zero);
+  };
   update = function(){
-    var left;
-    left = moment().countdown(config.deadline, config.flag);
-    $time.html(padding(left.hours, 2) + ":" + padding(left.minutes, 2) + ":" + padding(left.seconds, 2) + "." + padding(left.milliseconds, 3));
-    requestAnimationFrame(update);
+    var ref$, hours, minutes, seconds, milliseconds, value;
+    ref$ = moment().countdown(config.deadline, config.flag), hours = ref$.hours, minutes = ref$.minutes, seconds = ref$.seconds, milliseconds = ref$.milliseconds, value = ref$.value;
+    $time.html(padding(hours) + ":" + padding(minutes) + ":" + padding(seconds) + "." + padding(milliseconds, 3));
+    requestAnimationFrame(value <= 0 ? zero : update);
   };
   requestAnimationFrame(update);
 }).call(this);
--- src/ls/main.ls
+++ src/ls/main.ls
@@ -3,16 +3,24 @@ $time = $ '#time'
 config =
   deadline: '2014-08-03T1600'
   flag: countdown.HOURS .|. countdown.MINUTES .|. countdown.SECONDS .|. countdown.MILLISECONDS
+  ending: '00:00:00.000'
 
-padding = (num, len) ->
+padding = (num, len = 2) ->
   result = "#num"
   for from result.length til len
     result = "0#result"
   result
 
+counter = 0
+zero = !->
+  $time.html if counter < 10 then '' else config.ending
+  counter += 1
+  counter %= 20
+  request-animation-frame zero
+
 update = !->
-  left = moment!countdown config.deadline, config.flag
-  $time.html "#{padding left.hours, 2}:#{padding left.minutes, 2}:#{padding left.seconds, 2}.#{padding left.milliseconds, 3}"
-  request-animation-frame update
+  {hours, minutes, seconds, milliseconds, value} = moment!countdown config.deadline, config.flag
+  $time.html "#{padding hours}:#{padding minutes}:#{padding seconds}.#{padding milliseconds, 3}"
+  request-animation-frame if value <= 0 then zero else update
 
 request-animation-frame update
